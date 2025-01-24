a387fa6e6b031151e8296b76eb9c2310ed630439
Nileshsehrawat <nilesh.sehrawat16@gmail.com>
2025-01-24 21:13:02 +0530

    Primitives is added in java

---
+++ Java/Begineer/Primitives.java
@@ -0,0 +1,11 @@
+public class Primitives {
+    public static void main(String[] args){
+        int rollNO = 21;
+        char ch = 'N';
+        float marks = 83.7f;
+        double largeDecimalNumbers = 45823.2646;
+        long largeInteger = 789465321;
+        boolean check = true/false;
+
+    }
+}
