024bc920275219f83693892251648fd8ac62a790
nileshsehrawat <nilesh.sehrawat16@gmail.com>
2026-01-02 23:39:44 +0530

    Day 2

--- Java/PatternQuestions/AllPaterns.java
+++ Java/PatternQuestions/AllPaterns.java
@@ -1,8 +1,10 @@
 package Java.PatternQuestions;
 
+//this file consists of 22 patterns which covers majorly all of the cponcepts for logical thinking
+
 public class AllPaterns {
   public static void main(String[] args){
-  binaryRightTriangle(5);
+  reverseAlphabetsPattern(5);
     // System.out.println("hello");
   }
 
@@ -136,6 +138,91 @@ public class AllPaterns {
         else{
           System.out.print("0");
         }
+      }
+      System.out.println();
+    }
+  }
+  private static void vPattern(int rows){
+    for (int i = 1; i<=rows; i++){
+
+      for (int j=1; j<=i; j++){
+        System.out.print(j);
+      }
+      for(int k= 1; k<= 2* (rows- i); k++){
+        System.out.print(" ");
+      }
+      for(int j= i; j>= 1; j--){
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+  private static void incrementalTrianglePattern(int rows){
+    int a= 1;
+    for (int i = 1; i<=rows; i++){
+      for (int j=1; j<=i; j++){
+        System.out.print(a + " ");
+        a++;
+      }
+      System.out.println();
+    }
+  }
+  private static void alphabetsTrianglePattern(int rows){
+    for (int i = 1; i<=rows; i++){
+      char ch = 'A';
+      for (int j=1; j<=i; j++){
+        System.out.print(ch + " ");
+        ch++;
+      }
+      System.out.println();
+    }
+  }
+  private static void alphabetsReverseTrianglePattern(int rows){
+    for (int i = rows; i>=1; i--){
+      char ch = 'A';
+      for (int j= 1; j<=i; j++){
+        System.out.print(ch + " ");
+        ch++;
+      }
+      System.out.println();
+    }
+  }
+  private static void repeatAlphabetPattern(int rows){
+    char ch = 'A';
+    for (int i = 1; i<= rows; i++){
+      for (int j= 1; j<=i; j++){
+        System.out.print(ch + " ");
+      }
+      ch++;
+      System.out.println();
+    }
+  }
+  private static void alphabetsPyramidPattern(int rows){
+    for( int i= 1; i<= rows; i++){
+      //spaces
+      for (int j= 1; j<= rows - i; j++){
+        System.out.print(" ");
+      }
+      char ch= 'A'; 
+      //print
+      for (int k=1; k<= i; k++){
+        System.out.print(ch +" ");
+        ch++;
+      }
+      ch--;
+      for (int l= 1; l< i; l++){
+        ch--;
+        System.out.print(ch + " ");
+      }
+      System.out.println();
+    }
+  }
+  private static void reverseAlphabetsPattern(int rows){
+    for(int i= 1; i<= rows; i++){
+      char ch = (char) ('A' + rows - i);
+      for(int j= 1; j<= i; j++){
+        System.out.print(ch + " ");
+        ch++;
       }
       System.out.println();
     }
